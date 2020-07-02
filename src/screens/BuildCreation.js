3765cb017f57d5ced6cbfcf6e754f0cee9001d4e
YanisRili <yrili@outlook.fr>
2020-07-02 12:31:08 +0200

    add tous les arbres de runes

--- src/screens/BuildCreation.js
+++ src/screens/BuildCreation.js
@@ -148,17 +148,188 @@ export default class BuildCreation extends Component {
 
 
 
+                {this.state.isVisibleRouge ? 
+                    <View>
+                    <Picker
+                        style={{ height: 50, width: 150 }}
+                        onValueChange={(itemValue) => this.setState({rp1:itemValue})}
+                        >
+                        <Picker.Item label={this.state.rp1} value="" />
+                        <Picker.Item label="Electrocution" value="Electrocution" />
+                        <Picker.Item label="Prédateur" value="Prédateur" />
+                        <Picker.Item label="Moisson Noire" value="MoissonNoire" />
+                        <Picker.Item label="Déluge de lames" value="DélugeDeLames" />
+                    </Picker> 
+
+                    <Picker
+                        style={{ height: 50, width: 150 }}
+                        onValueChange={(itemValue) => this.setState({rp2:itemValue})}
+                        >
+                        <Picker.Item label={this.state.rp2} value="" />
+                        <Picker.Item label="Coup Bas" value="CoupBas" />
+                        <Picker.Item label="Goût du Sang" value="GoûtDuSang" />
+                        <Picker.Item label="Ruée Offensive" value="RuéeOffensive" />
+                    </Picker>
+
+                    <Picker
+                        style={{ height: 50, width: 150 }}
+                        onValueChange={(itemValue) => this.setState({rp3:itemValue})}
+                        >
+                        <Picker.Item label={this.state.rp3} value="" />
+                        <Picker.Item label="Balise Zombie" value="BaliseZombie" />
+                        <Picker.Item label="Poro Fantôme" value="PoroFantôme" />
+                        <Picker.Item label="Arracheur d'Oeil" value="ArracheurDOeil" />
+                    </Picker>
+
+                    <Picker
+                        style={{ height: 50, width: 150 }}
+                        onValueChange={(itemValue) => this.setState({rp4:itemValue})}
+                        >
+                        <Picker.Item label={this.state.rp4} value="" />
+                        <Picker.Item label="Chasseur Vorace" value="ChasseurVorace" />
+                        <Picker.Item label="Chasseur Ingénieux" value="ChasseurIngénieux" />
+                        <Picker.Item label="Chasseur Acharné" value="ChasseurAcharné" />
+                        <Picker.Item label="Chasseur Ultime" value="ChasseurUltime" />
+                    </Picker>
+                </View>
+                : null }
+                
+                {this.state.isVisibleBleu ? 
+                    <View>
+                    <Picker
+                        style={{ height: 50, width: 150 }}
+                        onValueChange={(itemValue) => this.setState({rp1:itemValue})}
+                        >
+                        <Picker.Item label={this.state.rp1} value="" />
+                        <Picker.Item label="Invocation d'Aery" value="InvocationDAery" />
+                        <Picker.Item label="Comète Arcanique" value="ComèteArcanique" />
+                        <Picker.Item label="Rush Phasique" value="RushPhasique" />                        
+                    </Picker> 
+
+                    <Picker
+                        style={{ height: 50, width: 150 }}
+                        onValueChange={(itemValue) => this.setState({rp2:itemValue})}
+                        >
+                        <Picker.Item label={this.state.rp2} value="" />
+                        <Picker.Item label="Orbe Anti-Magie" value="OrbeAntiMagie" />   
+                        <Picker.Item label="Ruban de Mana" value="RubanDeMana" />
+                        <Picker.Item label="Manteau Nuageux" value="ManteauNuageux" />
+                    </Picker>
+
+                    <Picker
+                        style={{ height: 50, width: 150 }}
+                        onValueChange={(itemValue) => this.setState({rp3:itemValue})}
+                        >
+                        <Picker.Item label={this.state.rp3} value="" />
+                        <Picker.Item label="Transcendance" value="Transcendance" />
+                        <Picker.Item label="Célérité" value="Célérité" />
+                        <Picker.Item label="Concentration Absolue" value="ConcentrationAbsolue" />
+                    </Picker>
+
+                    <Picker
+                        style={{ height: 50, width: 150 }}
+                        onValueChange={(itemValue) => this.setState({rp4:itemValue})}
+                        >
+                        <Picker.Item label={this.state.rp4} value="" />
+                        <Picker.Item label="Brûlure" value="Brûlure" />
+                        <Picker.Item label="Marche sur l'Eau" value="MarcheSurLEau" />
+                        <Picker.Item label="Tempête Menaçante" value="TempêteMenaçante" />
+                    </Picker>
+                </View>
+                : null }
+
+
+                {this.state.isVisibleVert ? 
+                    <View>
+                    <Picker
+                        style={{ height: 50, width: 150 }}
+                        onValueChange={(itemValue) => this.setState({rp1:itemValue})}
+                        >
+                        <Picker.Item label={this.state.rp1} value="" />
+                        <Picker.Item label="Poigne de l'Immortel" value="PoigneDeLImmortel" />
+                        <Picker.Item label="Après-Coup" value="AprèsCoup" />
+                        <Picker.Item label="Gardien" value="Gardien" />                        
+                    </Picker> 
+
+                    <Picker
+                        style={{ height: 50, width: 150 }}
+                        onValueChange={(itemValue) => this.setState({rp2:itemValue})}
+                        >
+                        <Picker.Item label={this.state.rp2} value="" />
+                        <Picker.Item label="Démolition" value="Démolition" />   
+                        <Picker.Item label="Fontaine de Vie" value="FontaineDeVie" />
+                        <Picker.Item label="Coup de Bouclier" value="CoupDeBouclier" />
+                    </Picker>
+
+                    <Picker
+                        style={{ height: 50, width: 150 }}
+                        onValueChange={(itemValue) => this.setState({rp3:itemValue})}
+                        >
+                        <Picker.Item label={this.state.rp3} value="" />
+                        <Picker.Item label="Conditionnement" value="Conditionnement" />
+                        <Picker.Item label="Second Souffle" value="Second Souffle" />
+                        <Picker.Item label="Plaque D'Os" value="PlaqueDOs" />
+                    </Picker>
+
+                    <Picker
+                        style={{ height: 50, width: 150 }}
+                        onValueChange={(itemValue) => this.setState({rp4:itemValue})}
+                        >
+                        <Picker.Item label={this.state.rp4} value="" />
+                        <Picker.Item label="Surcroissance" value="Surcroissance" />
+                        <Picker.Item label="Revitalisation" value="Revitalisation" />
+                        <Picker.Item label="Inébranlable" value="Inébranlable" />
+                    </Picker>
+                </View>
+                : null }
 
 
 
+                {this.state.isVisibleCyan ? 
+                    <View>
+                    <Picker
+                        style={{ height: 50, width: 150 }}
+                        onValueChange={(itemValue) => this.setState({rp1:itemValue})}
+                        >
+                        <Picker.Item label={this.state.rp1} value="" />
+                        <Picker.Item label="Optimisation Glaciale" value="OptimisationGlaciale" />
+                        <Picker.Item label="Grimoire Déchaîné" value="GrimoireDéchaîné" />
+                        <Picker.Item label="Prototype Omnipierre" value="PrototypeOmnipierre" />                        
+                    </Picker> 
 
+                    <Picker
+                        style={{ height: 50, width: 150 }}
+                        onValueChange={(itemValue) => this.setState({rp2:itemValue})}
+                        >
+                        <Picker.Item label={this.state.rp2} value="" />
+                        <Picker.Item label="Canaliportation Hextech" value="CanaliportaionHextech" />   
+                        <Picker.Item label="Chaussures Magiques" value="ChaussuresMagiques" />
+                        <Picker.Item label="Timing Parfait" value="TimingParfait" />
+                    </Picker>
 
+                    <Picker
+                        style={{ height: 50, width: 150 }}
+                        onValueChange={(itemValue) => this.setState({rp3:itemValue})}
+                        >
+                        <Picker.Item label={this.state.rp3} value="" />
+                        <Picker.Item label="Marché du Futur" value="MarchéDuFutur" />
+                        <Picker.Item label="Désintégrateur de Sbires" value="DésintégrateurDeSbires" />
+                        <Picker.Item label="Livraison de Biscuits" value="LivraisonDeBiscuits" />
+                    </Picker>
 
-
-
-
-
-                {this.state.isVisibleRouge ? <ArbreRouge /> : null }
+                    <Picker
+                        style={{ height: 50, width: 150 }}
+                        onValueChange={(itemValue) => this.setState({rp4:itemValue})}
+                        >
+                        <Picker.Item label={this.state.rp4} value="" />
+                        <Picker.Item label="Savoir Cosmique" value="SavoirCosmique" />
+                        <Picker.Item label="Vitesse d'Approche" value="VitesseDApproche" />
+                        <Picker.Item label="Philtre de Chronodistorsion" value="PhiltreDeChronodistorsion" />
+                    </Picker>
+                </View>
+                : null }
+                
+                
                 <Text>{this.state.test}</Text>
                 <TouchableOpacity
                 onPress={() => this._submit()}>
